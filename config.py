813d74eb61e664c3a6a36915e0d8b1541bedee57
Andrey Kras <atlanta.dedal@gmail.com>
2023-05-12 15:42:39 +0300

    ghb

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
